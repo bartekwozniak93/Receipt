ed8c76c93561d4d6038f503f267fb2198ea57aea
Bartek <bartekwozniak93@gmail.com>
2016-10-23 01:00:37 +0800

    New method editreceipt

--- controllers/receipt.js
+++ controllers/receipt.js
@@ -45,8 +45,6 @@ exports.editReceipt = function(req, res) {
     Receipt.findOne({ _id: req.body.receiptId }, function(err, receipt) {
         if (receipt) {
             receipt.title=req.body.title;
-            receipt.date=req.body.date;
-            receipt.eventId=req.body.eventId;
             receipt.description=req.body.description;
             receipt.total=req.body.total;
             receipt.users=[];
