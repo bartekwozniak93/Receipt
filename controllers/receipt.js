bcceb6f5174359528ed637cce5473648c877f8fe
Bartek <bartekwozniak93@gmail.com>
2016-10-23 00:29:47 +0800

    New method editreceipt

--- controllers/receipt.js
+++ controllers/receipt.js
@@ -1,67 +1,67 @@
 var Receipt = require('../models/receipt');
 
-exports.newReceipt = function (req, res) {
+exports.newReceipt = function(req, res) {
     var receipt = new Receipt();
-    receipt.title = req.body.title;
-    receipt.date = req.body.date;
-    receipt.eventId = req.body.eventId;
-    receipt.description = req.body.description;
-    receipt.total = req.body.total;
+    receipt.title=req.body.title;
+    receipt.date=req.body.date;
+    receipt.eventId=req.body.eventId;
+    receipt.description=req.body.description;
+    receipt.total=req.body.total;
     var arr = JSON.parse(req.body.users);
-    for (var i = 0; i < arr.length; i++) {
+    for(var i = 0; i<arr.length; i++) {
         receipt.users.push(arr[i]);
     }
-    receipt.save(function (err) {
+    receipt.save(function(err) {
         if (err)
             console.log(err);
         res.json(receipt);
     });
 };
 
-exports.getReceipts = function (req, res) {
-    Receipt.find({eventId: req.body.eventId}, function (err, receipts) {
+exports.getReceipts = function(req, res) {
+    Receipt.find({ eventId: req.body.eventId }, function(err, receipts) {
         if (!receipts) {
             res.json('There is no receipts.');
         } else {
-            res.json({"receipts": receipts});
+            res.json({"receipts":receipts});
         }
     });
 };
 
 
-exports.getReceipt = function (req, res) {
-    Receipt.findOne({_id: req.body.receiptId}, function (err, receipt) {
-        if (!receipt) {
-            res.json('There is no receipts.');
-        } else {
-            res.json({"receipt": receipt});
-        }
-    });
-};
-
-exports.editReceipt = function (req, res) {
+exports.getReceipt = function(req, res) {
     Receipt.findOne({_id: req.body.receiptId})
         .populate('users')
         .exec(function (err, receipt) {
             if (!receipt) {
-                receipt.title = req.body.title;
-                receipt.date = req.body.date;
-                receipt.eventId = req.body.eventId;
-                receipt.description = req.body.description;
-                receipt.total = req.body.total;
-                receipt.users = [];
-                var arr = JSON.parse(req.body.users);
-                for (var i = 0; i < arr.length; i++) {
-                    receipt.users.push(arr[i]);
-                }
-                receipt.save(function (err) {
-                    if (err)
-                        console.log(err);
-                    res.json(receipt);
-                });
+                res.json('There is no receipts.');
             } else {
-                res.json({"receipt": receipt});
+                res.json({"receipt":receipt});
             }
         });
+};
+
+exports.editReceipt = function(req, res) {
+    Receipt.findOne({ _id: req.body.receiptId }, function(err, receipt) {
+        if (!receipt) {
+            receipt.title=req.body.title;
+            receipt.date=req.body.date;
+            receipt.eventId=req.body.eventId;
+            receipt.description=req.body.description;
+            receipt.total=req.body.total;
+            receipt.users=[];
+            var arr = JSON.parse(req.body.users);
+            for(var i = 0; i<arr.length; i++) {
+                receipt.users.push(arr[i]);
+            }
+            receipt.save(function(err) {
+                if (err)
+                    console.log(err);
+                res.json(receipt);
+            });
+        } else {
+            res.json({"receipt":receipt});
+        }
+    });
 };
 
